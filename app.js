d89db478ce7d4470c5664a5451dcd2eda2125460
Jefferson Fernando da Silva Aguiar <156930825+Jeffersonday@users.noreply.github.com>
2024-02-07 16:33:13 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+alert('O Jogo do Número Secreto #')
+let numeroSecreto = parseInt(Math.random() * 10  + 1);
+console.log(numeroSecreto);
+let chute;
+let tentativas = 1;
+
+while (chute != numeroSecreto) {
+    chute = prompt('Digite um número entre 0 e 10');
+
+    if (chute == numeroSecreto) {
+        alert(`Você acertou o Número secreto: ${chute}`);
+        alert(`tentativas: ${tentativas}`);
+        break;
+    } else {
+        if (chute > numeroSecreto) {
+            alert(`O numero é menor que ${chute}`);
+        }
+        
+        if (chute < numeroSecreto) {
+            alert(`O numero é maior que ${chute}`);
+
+    }
+
+    }
+
+    tentativas++;
+    
+}  
+
+
+
+
+
+
+
+
+
