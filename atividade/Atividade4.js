ff17463315c92bb2d4875617481d9d778c8f3671
Jefferson Fernando da Silva Aguiar <156930825+Jeffersonday@users.noreply.github.com>
2024-02-07 16:51:03 -0300

    Delete atividade directory

--- atividade/Atividade4.js
+++
@@ -1,8 +0,0 @@
-alert ('Boas vindas ao jogo do número secreto')
-let chute = prompt('Escolha um número entre 1 e 10')
-
-let numeroSecreto = 4
-
-if (chute == numeroSecreto) {
-   alert ('Acertou')
-}
